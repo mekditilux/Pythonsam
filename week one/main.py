34fb4da60903b61c87678f017f4012498d844a52
mekditilux <mekdi.tilux@gmail.com>
2023-03-10 18:13:21 +0000

    week one lesson

---
+++ week one/main.py
@@ -0,0 +1,40 @@
+# this code is to add two numbers
+"""i just want practis python
+gnjdgj glkldjg pow
+fd; gjg fjgodlkjklsdj"""
+"""X=12
+Y=23
+z=X+Y
+print(z)"""
+#_name="Mekdes"
+#print(_name)
+
+"""x,y,z =10,20,30
+print(x)
+A=z+y
+B=x+y
+print (A)
+print(B)
+Fname='Mekdes'
+Lname='Asfaw''
+print (Fname+" "+Lname)"""
+
+x=23
+y='Yoseph'
+z=23.3
+m=True
+student=["kebed","lema","kasa"]
+c=complex(m)
+j=complex(x)
+M="this is Alen's book \"Wonderland place\""
+B="""kdjfdkgfndg;kdfkg
+gjnkdfg;sldkfjgskf
+fnglkdfmgb;ldmbldfmb;gkhbs hldfskhlkjklfjn
+jgdfkgjkdfgniodjngkdsfjbdsmfg
+fjnglkdsjfn"""
+greeting='Hello,world'
+name='Yoseph'
+message1=greeting+' '+name
+message='{},{}.welcome!'.format( greeting,name)
+message3= f'{greeting},{name}.welcome me!'
+print(greeting.split(","))
