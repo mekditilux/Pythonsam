4bedd47d4b2b950402fe496b3ea1fd7772e4e0fc
mekditilux <mekdi.tilux@gmail.com>
2023-03-12 16:17:47 +0000

    Function

--- Loop.py
+++ Loop.py
@@ -1,10 +1,32 @@
-while i<11:
-  
- print(i)
- i+=1
+# while loop
+"""i=1
+while i<6:
+    print(i)
+    i+=1"""
 
-else:
- print ("it is 11")"""
+#For loop
 
- #For loop
+# student=["Feven","Mose","Liya"]
+# print (student[])
+
+# # for s in student:
+# #     print(s)
+
+
+# name="Yoseph"
+# for y in enumerate(name):
+#     print(y)
+    
+
+# for c in range (2,30,3):
+#     print(c)
  
+# adj=["red", "big", "tasty"]
+# fruit=["apple", "banana","charry"]
+# for a in adj:
+#   for f in fruit:
+#     print(a,f)
+
+for x in range(0,1,2):
+    print(x)
+    pass
---
+++ function.py
@@ -0,0 +1,10 @@
+num1=12
+num2=23
+
+def sum(n1,n2):
+    return n1+n2
+def dif(n1,n2):
+    return n1-n2
+def pro(n1,n2):
+    return n1*n2
+print(sum(49,10))
